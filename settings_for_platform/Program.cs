a923a0343aa7bce4085402ad2ad227a81efcb5f5
Светлана Краснокутская <krasnokutskaya.sa@edu.spbstu.ru>
2020-04-24 00:22:07 +0300

    Изменения

---
+++ settings_for_platform/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace settings_for_platform
+{
+    abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input name OS:");
+            string type = Console.ReadLine();
+            switch (type)
+            {
+                case "ubuntu":
+                    client client1 = new client();
+                    client1.ubuntuSet();
+                    break;
+                case "Windows":
+                    client client2 = new client();
+                    client2.windowsSet();
+                    break;
+                case "macOS":
+                    client client3 = new client();
+                    client3.macOSSet();
+                    break;
+                default:
+                    Console.WriteLine("Error!"); break;
+            }
+                    Console.ReadKey();
+
+        }
+    }
+}
---
+++ settings_for_platform/client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace settings_for_platform
+{
+    /// <summary>
+    /// клиент
+    /// </summary>
+    class client
+    {
+        /// <summary>
+        /// настройка windows
+        /// </summary>
+        public void windowsSet()
+        {
+            cl(new windows_switcher());
+        }
+        /// <summary>
+        /// настройка macos
+        /// </summary>
+        public void macOSSet()
+        {
+            cl(new macos_switcher());
+        }
+        /// <summary>
+        /// настройка ubuntu
+        /// </summary>
+        public void ubuntuSet()
+        {
+            cl(new ubuntu_switcher());
+        }
+
+
+        /// <summary>
+        /// выводим настройки в зависимости от типа системы
+        /// </summary>
+        /// <param name="sw"></param>
+        public void cl(switcher sw)
+        {
+            sw.getSettings().Type();
+            sw.getSettings().GetSet1();
+            sw.getSettings().GetSet2();
+            sw.getSettings().GetSet3();
+            sw.getSettings().GetSet4();
+            sw.getSettings().GetSet5();
+            sw.getSettings().GetSet6();
+            sw.getSettings().GetSet7();
+            sw.getSettings().GetSet8();
+            sw.getSettings().GetSet9();
+            sw.getSettings().GetSet10();
+
+        }
+
+    }
+}
